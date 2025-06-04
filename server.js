64ac5a57bc6b3fdb47f4e031d4df84f5988f785c
arshak756 <mohammedarshak793@gmail.com>
2025-06-04 15:45:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 
 app.use(cors());
 app.use(express.json());
@@ -24,5 +25,6 @@ app.post("/api/login", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`✅ Server running on port ${PORT}`);
 });
+
